b6251be61dd0636f425e4e187f78ffb654f18207
ScriptSmith <ScriptSmith@users.noreply.github.com>
2017-01-31 17:34:27 +1000

    mock breaking dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -31,12 +31,18 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['PyQt5', 'socialreaper']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
