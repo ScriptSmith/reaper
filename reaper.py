84d92018ddb7054012e6cc44499dfb26adfd8bfb
ScriptSmith <ScriptSmith@users.noreply.github.com>
2017-03-07 12:41:52 +1000

    version update

--- reaper.py
+++ reaper.py
@@ -100,7 +100,7 @@ class Reaper(Ui_MainWindow):
     def __init__(self, window, show=True):
         super().__init__()
 
-        self.version = "v0.1.2"
+        self.version = "v0.1.3"
 
         self.auth_keys = {}
         self.download_details = {}
