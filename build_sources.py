bd684430a5fca4401825a590380e0d7eca8b5a42
Adam Smith <adamdevsmith@gmail.com>
2018-01-19 23:35:51 +1000

    Classes for arguments and setters

--- build_sources.py
+++ build_sources.py
@@ -5,8 +5,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 from socialreaper import Facebook
 
+
 class AdvancedBox(QtWidgets.QCheckBox):
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
 
         self.child_items = []
@@ -22,6 +23,129 @@ class AdvancedBox(QtWidgets.QCheckBox):
             item.setVisible(not item.isVisible())
 
 
+class ArgTableItem(QtWidgets.QTableWidgetItem):
+    def __init__(self, value, table, parent=None):
+        super().__init__()
+        QtWidgets.QWidget.__init__(self, parent)
+
+        self.setText(value)
+        self.table = table
+        self.old_value = value
+
+
+class ArgTableArg(ArgTableItem):
+    def __init__(self, value, table, pair, parent=None):
+        super().__init__(value, table, parent)
+        self.pair = pair
+
+
+class ArgTableVal(ArgTableItem):
+    def __init__(self, value, table, pair, parent=None):
+        super().__init__(value, table, parent)
+        self.pair = pair
+
+
+class ArgTablePair():
+    def __init__(self, key, value, table):
+        self.arg = ArgTableArg(key, table, self)
+        self.val = ArgTableVal(value, table, self)
+
+    def argument(self):
+        return self.arg
+
+    def value(self):
+        return self.val
+
+    def pair(self):
+        return self.arg, self.val
+
+
+class ArgTable(QtWidgets.QTableWidget):
+    def __init__(self, rows=None, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        super().__init__(0, 2)
+
+        self.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        self.verticalHeader().setVisible(False)
+        self.setHorizontalHeaderLabels(['Argument', 'Value'])
+
+        self.arguments = {}
+
+        self.itemChanged.connect(self.item_changed)
+
+        if rows:
+            self.read_rows(rows)
+
+        self.fill_table()
+
+    def read_rows(self, rows):
+        for row in rows:
+            argument = row[0].text
+            value = row[1].text
+            self.set_argument(argument, value)
+
+    def item_changed(self, item):
+        if isinstance(item, ArgTableArg):
+            old_arg = item.old_value
+            if old_arg != "":
+                value = self.arguments.pop(old_arg)
+            else:
+                value = item.pair.value().text()
+            new_key = item.text()
+            if new_key:
+                self.arguments[new_key] = value
+
+        elif isinstance(item, ArgTableVal):
+            argument = item.pair.argument().text()
+            value = item.text()
+            self.arguments[argument] = value
+
+        self.fill_table()
+
+    def set_argument(self, arg, value):
+        self.arguments[arg] = value
+
+    def remove_argument(self, arg):
+        del self.arguments[arg]
+
+    def inc_rows(self):
+        self.setRowCount(self.rowCount() + 1)
+        return self.rowCount()
+
+    def add_row(self, argument, value):
+        self.itemChanged.disconnect(self.item_changed)
+        rowCount = self.inc_rows()
+
+        argItem, valItem = ArgTablePair(str(argument), str(value), self).pair()
+
+        self.setItem(rowCount - 1, 0, argItem)
+        self.setItem(rowCount - 1, 1, valItem)
+        self.itemChanged.connect(self.item_changed)
+
+    def fill_table(self):
+        self.setRowCount(0)
+        for key, value in self.arguments.items():
+            self.add_row(key, value)
+
+        self.add_row("", "")
+
+
+class CounterSetter(QtWidgets.QSpinBox):
+    def __init__(self, value, table, argument, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+
+        self.setMinimum(0)
+        self.setMaximum(sys.maxsize)
+
+        self.table = table
+        self.argument = argument
+        self.valueChanged.connect(self.set_arg)
+        self.setValue(value)
+
+
+    def set_arg(self, value):
+        self.table.set_argument(self.argument, value)
+        self.table.fill_table()
 
 
 def tree_handler(item, column_no):
@@ -32,6 +156,7 @@ def table_add_item(text, parent, row, col):
     newItem = QtWidgets.QTableWidgetItem(text)
     newItem.parent = parent
     parent.setItem(row, col, newItem)
+
 
 def table_cell_changed(item):
     parent = item.parent
@@ -113,6 +238,7 @@ def add_nodes(sourceName, parentNode, treeWidget, sourceDescription, textDescrip
         treeItem = QtWidgets.QTreeWidgetItem(treeWidget)
         treeItem.sourceDescription = sourceDescription
         treeItem.setText(0, name)
+        treeItem.setExpanded(True)
 
         # Assign treeItem a pageIndex
         treeItem.pageIndex = pageIndex
@@ -202,7 +328,11 @@ def add_nodes(sourceName, parentNode, treeWidget, sourceDescription, textDescrip
                     listItem = QtWidgets.QListWidgetItem(elem.text, listBox)
                     listBox.addItem(listItem)
                 rowContent = listBox
-            elif inputType == "table":
+            elif inputType == "arguments":
+                rows = input.find('rows')
+
+                tableBox = ArgTable(rows)
+
                 setters = input.find('setters')
                 if setters:
                     for setter in setters:
@@ -211,32 +341,8 @@ def add_nodes(sourceName, parentNode, treeWidget, sourceDescription, textDescrip
                         setterValue = setter.find('value').text
 
                         if setterType == "counter":
-                            setCounter = QtWidgets.QSpinBox(inputBox)
-                            setCounter.setMinimum(0)
-                            setCounter.setMaximum(sys.maxsize)
-                            setCounter.setValue(int(setterValue))
+                            setCounter = CounterSetter(int(setterValue), tableBox, "count")
                             inputBox.layout.addRow(setterName, setCounter)
-
-                columns = input.find('columns')
-                rows = input.find('rows')
-                if not rows:
-                    rows = []
-
-                tableBox = QtWidgets.QTableWidget(len(rows) + 1, len(columns), inputBox)
-                tableBox.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
-                tableBox.verticalHeader().setVisible(False)
-                tableBox.setHorizontalHeaderLabels([column.text for column in columns])
-
-                row_c = 0
-                for row_c, row in enumerate(rows):
-                    for cell_c, cell in enumerate(row):
-                        table_add_item(cell.text, tableBox, row_c, cell_c)
-
-                row_c = -1 if row_c == 0 else 0 # Create first empty row if table has no rows
-                for cell_c in range(len(columns)):
-                    table_add_item("", tableBox, row_c + 1, cell_c)
-
-                tableBox.itemChanged.connect(table_cell_changed)
 
                 rowContent = tableBox
 
@@ -248,7 +354,6 @@ def add_nodes(sourceName, parentNode, treeWidget, sourceDescription, textDescrip
 
             if not rowContent.required:
                 advancedBox.addRow(rowLabel, rowContent)
-
 
         pageDescription.layout.addWidget(inputBox)
 
