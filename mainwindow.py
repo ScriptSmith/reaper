311dbb62f390f4ed1b7be42051f2c7b7794024ca
Adam Smith <adamdevsmith@gmail.com>
2018-02-22 21:25:42 +1000

    ui update

--- mainwindow.py
+++ mainwindow.py
@@ -330,7 +330,7 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuHelp.menuAction())
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(2)
         self.sourcesTabs.setCurrentIndex(-1)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
